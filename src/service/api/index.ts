08fa039414d34306bba2b17f6995508e804dcdd5
chosooyeon <1whtndus@naver.com>
2022-08-22 23:38:43 +0900

    [ADD]config Auth

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -1,1 +1,16 @@
-export {}
+class Auth {
+    login = () => {
+        localStorage.setItem('token', '1')
+    }
+    isLogin = () => {
+        return localStorage.getItem('token') ? true : false
+    }
+    register = () => {
+        
+    }
+    logout = () => {
+        localStorage.removeItem('token');
+    }
+}
+
+export default new Auth()
